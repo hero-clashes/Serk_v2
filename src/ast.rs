52bd4bea7462422e17163b61fb3faf72883f1cdb
hero2002 <abdkafi2002@gmail.com>
2024-04-18 12:24:38 +0300

    added math expr  and string let

--- src/ast.rs
+++ src/ast.rs
@@ -1,16 +1,29 @@
+use std::io::SeekFrom;
+
 
 #[derive(Debug)]
 pub enum Statement{
     Comment,
     VariableDecl(String,String, Box<Vec<Statement>>),
+    Math(Box<Statement>, char, Box<Statement>),
+    Num(i64),
+    StringLet(String)
 }
 #[derive(Debug)]
 pub enum AST{
-    Function(String,Vec<(String,String)>,Vec<Statement>)
+    Function(String,String,Vec<(String,String)>,Vec<Statement>)
 }
 
 
+impl AST{
+    pub fn conv(&self) {
+        match self{
+            AST::Function(name,ty, args, stats) => {
 
+            },
+        }
+    }
+}
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 pub mod ast;
+use std::{fs::File, io::Read};
+
 use lalrpop_util::{lalrpop_mod, state_machine::ParseError};
 
 lalrpop_mod!(pub grammer);
-use inkwell::context::Context;
+use inkwell::{builder, context::Context, module};
 
 fn main() {
-    let input = "fn Something(any key,word some){
-        //None sense Comment
-        var s;
-    }";
-    let output = grammer::ModuleParser::new().parse(input);
+    let mut input = String::new();
+    let _ = File::open("tests/test.serk").unwrap().read_to_string(&mut input);
+    let output = grammer::ModuleParser::new().parse(&input);
     match output {
-        Ok(s) => println!("{:?}", s),
+        Ok(s) => {
+            println!("{:?}", s);
+            let context  = Context::create();
+            let module = context.create_module("Hero");
+            let builder = context.create_builder();
+            
+        }
         Err(s) => {
             println!("Error: {:?}", s);
-            match s{
-                lalrpop_util::ParseError::InvalidToken { location } => println!("{:?}",input.split_at(location)),
+            match s {
+                lalrpop_util::ParseError::InvalidToken { location } => {
+                    println!("{:?}", input.split_at(location))
+                }
                 lalrpop_util::ParseError::UnrecognizedEof { location, expected } => todo!(),
-                lalrpop_util::ParseError::UnrecognizedToken { token, expected } => println!("{:?}",input.split_at(token.0)),
+                lalrpop_util::ParseError::UnrecognizedToken { token, expected } => {
+                    println!("{:?}", input.split_at(token.0))
+                }
                 lalrpop_util::ParseError::ExtraToken { token } => todo!(),
                 lalrpop_util::ParseError::User { error } => todo!(),
             }
