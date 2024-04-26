31853d0fd0e3c641eabf768014bb39189579871c
hero2002 <abdkafi2002@gmail.com>
2024-04-26 23:02:36 +0300

    Added Prefix

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use argh::FromArgs;
 /// The Serk Complier
 struct Input {
     ///file to be complied/JITed
-    #[argh(option, default = "String::from(\"tests/test4.serk\")")]
+    #[argh(option, default = "String::from(\"tests/test5.serk\")")]
     pub file: String,
 }
 
